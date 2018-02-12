3c6db3a16fefa52b3431bc5948c54842335f1b03
Michael Bischoff <Michael.Bischoff@Bertelsmann.de>
2018-02-12 18:13:26 +0100

    add license headers per file. add feature 'send test email'

--- t9t-bpmn2-be/src/main/sql/POSTGRES/ForeignKey/engine_create_foreign_keys.sql
+++ t9t-bpmn2-be/src/main/sql/POSTGRES/ForeignKey/engine_create_foreign_keys.sql
@@ -1,3 +1,19 @@
+--
+-- Copyright (c) 2012 - 2018 Arvato Systems GmbH
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
 alter table ACT_GE_BYTEARRAY add constraint ACT_FK_BYTEARR_DEPL foreign key (DEPLOYMENT_ID_) references ACT_RE_DEPLOYMENT (ID_);
 
 alter table ACT_RU_EXECUTION add constraint ACT_FK_EXE_PROCINST foreign key (PROC_INST_ID_) references ACT_RU_EXECUTION (ID_);
